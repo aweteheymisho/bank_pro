656f2315925ff1c4ab2142f142050807d70148a1
aweteheymisho <aweteheymisho22@gmail.com>
2026-04-25 01:38:18 -0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" href="css/style.css">
+<title>Bank System</title>
+</head>
+
+<body>
+
+<h1>Simple Banking System</h1>
+
+<a href="register.php">Register</a>
+<a href="login.php">Login</a>
+
+</body>
+</html>
